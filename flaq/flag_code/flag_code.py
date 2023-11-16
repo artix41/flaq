d179cd4103bd677325a8da632d66d3a2cabb10c8
Arthur Pesah <arthur.pesah@gmail.com>
2023-11-16 13:15:15 +0700

    Create coloured graph of flags

--- flaq/flag_code/flag_code.py
+++ flaq/flag_code/flag_code.py
@@ -1,14 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 import networkx as nx
-from panqec.codes import Toric2DCode
+from panqec.codes import Toric2DCode, Planar2DCode
 
 
 class FlagCode:
-    def __init__(self, boundaryOperators):
+    def __init__(self, boundaryOperators, cellPositions=None):
         self.boundaryOperators = boundaryOperators
+        self.cellPositions = cellPositions
         self.dimension = len(self.boundaryOperators)
-
-        print("Boundary operators\n", boundaryOperators)
 
         for i in range(self.dimension-1):
             if self.boundaryOperators[i].shape[1] != self.boundaryOperators[i+1].shape[0]:
@@ -20,8 +21,11 @@ class FlagCode:
         for i in range(self.dimension):
             self.nCells.append(boundaryOperators[i].shape[1])
 
+        # Find all the flags recursively
+
         self.flags = []
         self.flagToIndex = dict()
+        self.flagCoordinates = []
 
         def getCompletedFlags(flagBeginning):
             if len(flagBeginning) == self.dimension+1:
@@ -39,15 +43,86 @@ class FlagCode:
             for flag in flags:
                 self.flagToIndex[flag] = len(self.flags)
                 self.flags.append(flag)
+                cellCoords = [cellPositions[i][flag[i]] for i in range(self.dimension+1)]
+                flagCoords = np.mean(cellCoords, axis=0)
+                self.flagCoordinates.append(flagCoords)
+
+        # Build the colored graph of flags by exploring it recursively
+
+        def getAdjacentFlags(flag, level):
+            print(level)
+            leftAdjacentFlags = None
+            rightAdjacentFlags = None
+
+            if level > 0:
+                leftAdjacentFlags = set(
+                    self.boundaryOperators[level-1][flag[level-1]].nonzero()[1]
+                )
+
+            if level < self.dimension:
+                rightAdjacentFlags = set(
+                    self.boundaryOperators[level].T[flag[level+1]].nonzero()[1]
+                )
+
+            print("LR", leftAdjacentFlags, rightAdjacentFlags)
+
+            if leftAdjacentFlags is None:
+                newCells = rightAdjacentFlags
+            elif rightAdjacentFlags is None:
+                newCells = leftAdjacentFlags
+            else:
+                newCells = leftAdjacentFlags.intersection(rightAdjacentFlags)
+
+            adjacentFlags = [(*flag[:level], cell, *flag[level+1:]) for cell in newCells]
+
+            return adjacentFlags
+
+        self.flagAdjacency = np.zeros((len(self.flags), len(self.flags)), dtype=np.uint8)
+        exploredFlags = set()
+
+        def exploreGraphFromFlag(flag):
+            print("Current flag", flag)
+            if flag in exploredFlags:
+                return
+
+            exploredFlags.add(flag)
+
+            for level in range(self.dimension+1):
+                for nextFlag in getAdjacentFlags(flag, level):
+                    if nextFlag != flag:
+                        print("Adj flags", nextFlag)
+                        idx1, idx2 = self.flagToIndex[flag], self.flagToIndex[nextFlag]
+                        self.flagAdjacency[idx1, idx2] = level + 1
+                        self.flagAdjacency[idx2, idx1] = level + 1
+
+                        exploreGraphFromFlag(nextFlag)
+
+        exploreGraphFromFlag(self.flags[0])
+
+        graph = nx.from_numpy_array(self.flagAdjacency)
+
+        edges, weights = zip(*nx.get_edge_attributes(graph, 'weight').items())
+
+        nx.draw(
+            graph, self.flagCoordinates, node_color='black', edge_color=np.array(weights)-1,
+            edge_cmap=plt.cm.tab20b, node_size=40, width=2
+        )
+
+        plt.show()
 
 
 if __name__ == "__main__":
-    code = Toric2DCode(3)
+    code = Planar2DCode(4)
     boundaryOperators = [
         code.Hz.todense(),
         code.Hx.todense().T
     ]
+    positions = [
+        np.array(code.stabilizer_coordinates)[code.z_indices],
+        code.qubit_coordinates,
+        np.array(code.stabilizer_coordinates)[code.x_indices]
+    ]
 
-    flagCode = FlagCode(boundaryOperators)
-    print(flagCode.flags)
-    print("Number of flags", len(flagCode.flags))
+    flagCode = FlagCode(boundaryOperators, positions)
+    # print(flagCode.flags)
+    # print("Number of flags", len(flagCode.flags))
